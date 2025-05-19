187cab8ab111ed7f540a9a1dbeb0018339958796
유큰길 <163965664+DAEYOUNG01@users.noreply.github.com>
2025-05-19 13:03:44 +0900

    Create T5_model.py

---
+++ T5_model.py
@@ -0,0 +1,19 @@
+from transformers import AutoModelForSeq2SeqLM,AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "wisenut-nlp-team/KoT5", 
+    revision="paraphrase",
+    token="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"
+)
+model = AutoModelForSeq2SeqLM.from_pretrained(
+    "wisenut-nlp-team/KoT5", 
+    revision="paraphrase",
+    token="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"
+)
+
+
+
+# 모델 저장 함수 (필요 시)
+def save_model():
+    model.save_pretrained("./koT5_correction_model")
+    tokenizer.save_pretrained("./koT5_correction_model")
